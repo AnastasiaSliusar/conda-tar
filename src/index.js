0ca3598517503104f1e3e5f545d0937ef21e1195
Anastasiia Sliusar <anastasiia.sliusar@gmail.com>
2024-10-22 15:05:50 +0200

    Transform to a class

--- src/index.js
+++ src/index.js
@@ -4,48 +4,73 @@ import initializeWasm from './helper';
 const condaPackageUrl = 'https://conda.anaconda.org/conda-forge/linux-64/_libgcc_mutex-0.1-conda_forge.tar.bz2';
 //const condaPackageUrl = "http://localhost:8888/_r-mutex-1.0.0-mro_2.conda";
 
-async function fetchByteArray(url) {
-    let response = await fetch(url)
-    if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
+
+export class Unpack {
+    constructor() {
+        this._wasmModule = null;
+        this._init();
     }
-    let arrayBuffer = await response.arrayBuffer();
-    let byte_array = new Uint8Array(arrayBuffer);
-    return byte_array;
-}
 
-async function unpackCondaFile() {
-    try {
-        const wasmModule = await initializeWasm();
-        let data = await fetchByteArray(condaPackageUrl);
-        console.log('Data downloaded:', data);
-
-        if (wasmModule) {
-            const inputPtr = wasmModule._malloc(data.length);
-            wasmModule.HEAPU8.set(data, inputPtr);
-            const outputSizePtr = wasmModule._malloc(data.length);
-
-            const extractedDataPtr = wasmModule._extract_archive(inputPtr, data.length, outputSizePtr);
-
-            const extractedSize = wasmModule.getValue(outputSizePtr, 'i32');
-
-            console.log('Extracted size:', extractedSize);
-
-            if (extractedDataPtr === 0) {
-                throw new Error('Archive extraction failed.');
-            }
-
-            const extractedData = new Uint8Array(wasmModule.HEAPU8.subarray(extractedDataPtr, extractedDataPtr + extractedSize));
-
-            wasmModule._free(inputPtr);
-            wasmModule._free(outputSizePtr);
-            wasmModule._free(extractedDataPtr);
-
-            return extractedData;
+    async fetchByteArray(url) {
+        let response = await fetch(url)
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
         }
-    } catch (error) {
-        console.log('Error during unpacking:', error);
+        let arrayBuffer = await response.arrayBuffer();
+        let byte_array = new Uint8Array(arrayBuffer);
+        return byte_array;
+    }
+
+    async _init() {
+        this._wasmModule = await initializeWasm();
+    }
+
+    extractData(data) {
+        let extractedData;
+        try {
+            if (!this._wasmModule) {
+                throw new Error('WASM module not initialized.');
+            }
+            if (this._wasmModule) {
+                const inputPtr = this._wasmModule._malloc(data.length);
+                this._wasmModule.HEAPU8.set(data, inputPtr);
+                const outputSizePtr = this._wasmModule._malloc(data.length);
+
+                const extractedDataPtr = this._wasmModule._extract_archive(inputPtr, data.length, outputSizePtr);
+
+                const extractedSize = this._wasmModule.getValue(outputSizePtr, 'i32');
+
+                console.log('Extracted size:', extractedSize);
+
+                if (extractedDataPtr === 0) {
+                    throw new Error('Archive extraction failed.');
+                }
+
+                extractedData = new Uint8Array(this._wasmModule.HEAPU8.subarray(extractedDataPtr, extractedDataPtr + extractedSize));
+
+                this._wasmModule._free(inputPtr);
+                this._wasmModule._free(outputSizePtr);
+                this._wasmModule._free(extractedDataPtr);
+
+                return extractedData;
+            }
+        } catch (error) {
+            console.log('Error during unpacking:', error);
+        }
+    }
+
+    async unpackCondaFileByUrl(url) {
+        let extractedData;
+        try {
+            let data = await this.fetchByteArray(url);
+            console.log('Data downloaded:', data);
+
+            extractedData = this.extractData(data);
+        } catch (error) {
+            console.error('Error during unpacking:', error);
+        }
+        return extractedData;
     }
 }
 
-unpackCondaFile();
+export default Unpack;
