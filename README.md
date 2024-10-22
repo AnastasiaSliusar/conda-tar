59cc61fab1415872748627c35a7584703c361672
Anastasiia Sliusar <anastasiia.sliusar@gmail.com>
2024-10-22 14:37:11 +0200

    Clean code

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
-## Conta-tar
+## Unpack
 Fetching and unpacking conda packages
-
-## Attribution
-
-Portions of this code are derived from the [SheetJS/bz2](https://github.com/sheetjs/bz2) repository, licensed under the Apache License 2.0.
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,8 @@
 
-import tar from 'tar-stream';
-import { decompress } from './bz2';
 import initializeWasm from './helper';
 
 const condaPackageUrl = 'https://conda.anaconda.org/conda-forge/linux-64/_libgcc_mutex-0.1-conda_forge.tar.bz2';
 //const condaPackageUrl = "http://localhost:8888/_r-mutex-1.0.0-mro_2.conda";
-
-function decompressBzip2(data) {
-    return decompress(data);
-}
 
 async function fetchByteArray(url) {
     let response = await fetch(url)
@@ -18,51 +12,6 @@ async function fetchByteArray(url) {
     let arrayBuffer = await response.arrayBuffer();
     let byte_array = new Uint8Array(arrayBuffer);
     return byte_array;
-}
-
-export async function fetchAndUntarBz2Package(url) {
-    try {
-        console.log('>??');
-
-        const compressedData = await fetchByteArray(url);
-        console.log(compressedData);
-        const decompressedData = decompressBzip2(compressedData);
-        const extract = tar.extract();
-        const files = {};
-
-        extract.on('entry', function (header, stream, next) {
-            const fileName = header.name;
-            let fileContent = '';
-            console.log('fileName');
-            console.log(fileName);
-            stream.on('data', function (chunk) {
-                fileContent += new TextDecoder().decode(chunk);
-            });
-
-            stream.on('end', function () {
-                files[fileName] = fileContent;
-                next();
-            });
-
-
-            stream.on('error', function (err) {
-                console.error(`Error reading stream for ${fileName}:`, err);
-                next(err);
-            });
-        });
-
-        extract.on('finish', function () {
-            console.log('All files extracted:', files);
-        });
-
-        extract.write(decompressedData);
-        extract.end();
-
-        return files;
-    } catch (error) {
-        console.error('Error fetching or untarring the package:', error);
-        throw error;
-    }
 }
 
 async function unpackCondaFile() {
